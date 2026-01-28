d6dc18ff7420e23301bd863191ffe53ed42a29b5
natdanai <email--natdanai.yuye@sbac.ac.th>
2026-01-28 15:47:38 +0700

    v2

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.get("/", (req,res) => {
+app.use(express.json());
+
+let department = [
+    { id: 1, name: "IT" },
+    { id: 2, name: "DBT" },
+];
+
+app.get("/", (req, res) => {
     res.send("Mr.Watthana puangthaisong");
+});
+
+app.get("/department", (req, res) => {
+    res.send(department);
+});
+
+app.post("/department", (req, res) => {
+    let newDepartment = {
+        id: department.length + 1,
+        name: req.body.name,
+    };
+    department.push(newDepartment);
+    res.send(department);
 });
 
 app.listen(port, () => {
     console.log("Server Starting on port", port);
 });
-
