3be0461b3ebf67954a91cb205b26526bb698c061
natdanai <email--natdanai.yuye@sbac.ac.th>
2026-01-28 14:56:07 +0700

    v1

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res) => {
+    res.send("Mr.Watthana puangthaisong");
+});
+
+app.listen(port, () => {
+    console.log("Server Starting on port", port);
+});
+
